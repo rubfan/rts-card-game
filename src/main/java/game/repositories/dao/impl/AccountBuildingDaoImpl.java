2270de8a73c38912a4fc550403574edd9466d21b
lupolov-ivan <ivannaib2012@gmail.com>
2018-05-01 22:06:48 +0300

    added missed letter

--- src/main/java/game/repositories/dao/impl/AccountBuildingDaoImpl.java
+++ src/main/java/game/repositories/dao/impl/AccountBuildingDaoImpl.java
@@ -19,7 +19,7 @@ public class AccountBuildingDaoImpl implements AccountBuildingDao {
         new QueryHelper() {
             protected void executeQuery(Statement statement, Connection connection) throws SQLException {
                 statement.executeUpdate("use card_battle_rts");
-                ResultSet rs = statement.executeQuery("elect * from Account_Building WHERE account_id = " + accountId);
+                ResultSet rs = statement.executeQuery("select * from Account_Building WHERE account_id = " + accountId);
                 while(rs.next()) {
                     AccountBuildingEntity accountBuilding = new AccountBuildingEntity(
                             rs.getInt("account_id"),
