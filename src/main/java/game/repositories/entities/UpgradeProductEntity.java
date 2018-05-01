34be3e37655df31c4b351460edca0488d5eddc1b
rickdemonn <rickdemonn@gmail.com>
2018-05-01 18:43:58 +0300

    ResourceQuantityEntity UpgradeProductEntity

---
+++ src/main/java/game/repositories/entities/UpgradeProductEntity.java
@@ -0,0 +1,50 @@
+package game.repositories.entities;
+
+import java.util.List;
+
+public class UpgradeProductEntity {
+    private UpgradeEntity upgradeEntity;
+    private List<ResourceEntity> resourceEntityList;
+    private List<BuildingEntity> buildingEntities;
+
+    public UpgradeProductEntity(UpgradeEntity upgradeEntity, List<ResourceEntity> resourceEntityList, List<BuildingEntity> buildingEntities) {
+        this.upgradeEntity = upgradeEntity;
+        this.resourceEntityList = resourceEntityList;
+        this.buildingEntities = buildingEntities;
+    }
+
+    UpgradeProductEntity(){}
+
+    public UpgradeEntity getUpgradeEntity() {
+        return upgradeEntity;
+    }
+
+    public void setUpgradeEntity(UpgradeEntity upgradeEntity) {
+        this.upgradeEntity = upgradeEntity;
+    }
+
+    public List<ResourceEntity> getResourceEntityList() {
+        return resourceEntityList;
+    }
+
+    public void setResourceEntityList(List<ResourceEntity> resourceEntityList) {
+        this.resourceEntityList = resourceEntityList;
+    }
+
+    public List<BuildingEntity> getBuildingEntities() {
+        return buildingEntities;
+    }
+
+    public void setBuildingEntities(List<BuildingEntity> buildingEntities) {
+        this.buildingEntities = buildingEntities;
+    }
+
+    @Override
+    public String toString() {
+        return "UpgradeProductEntity{" +
+                "upgradeEntity=" + upgradeEntity +
+                ", resourceEntityList=" + resourceEntityList +
+                ", buildingEntities=" + buildingEntities +
+                '}';
+    }
+}
