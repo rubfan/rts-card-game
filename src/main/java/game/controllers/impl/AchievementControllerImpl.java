02b88d36aaa7884f0ebcf65ce3c3a34ff4db14e5
Jamesson <vladun4ic@gmail.com>
2018-05-08 19:43:28 +0300

    Achievement1.1

--- src/main/java/game/controllers/impl/AchievementControllerImpl.java
+++ src/main/java/game/controllers/impl/AchievementControllerImpl.java
@@ -12,7 +12,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
-    @Path("/achievement")
+    @Path("/achievements")
     public class AchievementControllerImpl implements AchievementController {
 
 
