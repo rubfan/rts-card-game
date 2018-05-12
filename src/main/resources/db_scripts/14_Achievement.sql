e2658cd92b21210f9dae360bf9bd1cb702458f29
Jamesson <vladun4ic@gmail.com>
2018-05-12 21:09:41 +0300

    Achievement DB

--- src/main/resources/db_scripts/14_Achievement.sql
+++ src/main/resources/db_scripts/14_Achievement.sql
@@ -1,5 +1,20 @@
 INSERT INTO `Achievement`
     (`id`,`name`,`description`)
 VALUES
-    ('1','Mad Scrooge','Collect 10000 of corn'),
-    ('2','Mad Scrooge','Collect 10000 of corn');
+    ('1','Mad Scrooge','Collect 10000 of corn(in all)'),
+    ('2','Bazoookas Boy','Spend 200 cocks with bazookas'),
+    ('3','Shity Kingdom','Collect 1000 cockshit'),
+    ('4','First step to Win','Use your first battle card'),
+    ('5','Arow in tht Knee','Spend 100 Cock-Archers'),
+    ('6','Whats up Satan','Use 666 Wizards'),
+    ('7','Pick-up master','Collect 10000 Chicks(in all)'),
+    ('8','Cocks Fight club','use 150 fighting cock'),
+    ('9','300 Cock-Spartan','use 300 cock with swears'),
+    ('10','And you think this is a big gun?','make 100 Guns'),
+    ('11','I see dead cocks...','Make 100 cock-prophet'),
+    ('12','I am 12 and i love Minecraft','Craft(use) 2000 Stones and Wood'),
+    ('13','Lets make Booooom','Collect 2000 gunpowder(in all)'),
+    ('14','I have enough steel to be an Ironman','Collect 5000 of steel(in storege)'),
+    ('15','Eggs better that War','Collect 5000 of eggs(in all)');
+
+
