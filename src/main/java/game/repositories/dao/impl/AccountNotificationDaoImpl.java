e397fd03c72952471b690b5b9d543d6cc894a31c
Slava <vve1991@gmail.com>
2018-05-18 11:23:14 +0300

    AccountNotificationDao fix

--- src/main/java/game/repositories/dao/impl/AccountNotificationDaoImpl.java
+++ src/main/java/game/repositories/dao/impl/AccountNotificationDaoImpl.java
@@ -41,17 +41,17 @@ public class AccountNotificationDaoImpl implements AccountNotificationDao {
                 List<NotificationEntity> recentNotifications = new LinkedList<>();
                 ResultSet rs = statement.executeQuery(
                         "SELECT notification_id, name, description FROM\n" +
-                                "  (SELECT trigger_notification.notification_id, name, description,\n" +
+                                "  (SELECT Trigger_Notification.notification_id, name, description,\n" +
                                 "                            building_id, building_number,\n" +
                                 "                            resource_id, resource_number,\n" +
                                 "                            upgrade_id, upgrade_number\n" +
                                 "FROM\n" +
                                 "  (SELECT Notification.id,name,description\n" +
                                 "      FROM Notification LEFT JOIN\n" +
-                                "        (SELECT * FROM Account_Notification WHERE account_id = 1) AS account_shown\n" +
+                                "        (SELECT * FROM Account_Notification WHERE account_id = " + accountId + ") AS account_shown\n" +
                                 "        ON Notification.id = account_shown.notification_id\n" +
                                 "        WHERE account_shown.notification_id IS NULL) AS account_unshown\n" +
-                                "        LEFT JOIN trigger_notification\n" +
+                                "        LEFT JOIN Trigger_Notification\n" +
                                 "        ON account_unshown.id = Trigger_Notification.notification_id) as to_check\n" +
                                 "WHERE (to_check.building_number <= (SELECT number FROM Account_Building WHERE account_id = " + accountId + " AND building_id = to_check.building_id))\n" +
                                 "AND (to_check.resource_number <= (SELECT number FROM Account_Resource WHERE account_id = " + accountId + " AND resource_id = to_check.resource_id))\n" +
