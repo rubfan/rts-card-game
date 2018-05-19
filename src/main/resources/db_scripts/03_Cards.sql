b75e6e6118b14488df783d5abb03e77665c013e6
EvheniyHritsaenko <zhenya089@gmail.com>
2018-05-20 01:35:41 +0300

    Upgrade_Database

--- src/main/resources/db_scripts/03_Cards.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `Card`
-    (`name`,`description`)
-VALUES
-    ('Build Granary','Build one Granary l'),
-    ('Upgrade Granary','Upgrade Granary by 1 lvl'),
-    ('Master`s feast','Kill Enemy`s all eggs, chickens, cocks'),
-    ('Attack B cocks','Attack Enemy by Battle cocks');
--- src/main/resources/db_scripts/10_Upgrades_Product.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `Upgrade_Product`
-   (`id`,`upgrade_id`, `building_id`,`resource_id`, `percent`)
-VALUES
-   ('1','1','1','1','5'),
-   ('2','2','1','1','5'),
-   ('3','2','2','2','5');
