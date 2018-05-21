9f56e494cff8f7676e3b109969e74095f59e1d4a
rickdemonn <rickdemonn@gmail.com>
2018-05-21 21:01:10 +0300

    Start CardApply

--- src/main/java/game/services/AccountCardService.java
+++ src/main/java/game/services/AccountCardService.java
@@ -4,5 +4,5 @@ import java.util.List;
 
 public interface AccountCardService {
     List<Integer> getAllowAccountCards(Integer accountId);
-
+    void cardApply(Integer accountId, Integer cardId);
 }
