973e356f8bf4126ec26c39b7e4e1fe50347e7782
EvheniyHritsaenko <zhenya089@gmail.com>
2018-05-29 23:13:05 +0300

    Update_DB(Cards_Product-Upgrade+Action)

---
+++ src/main/resources/db_scripts/11_Notifications.sql
@@ -0,0 +1,6 @@
+INSERT INTO `Notification`
+    (`id`, `name`,`description`)
+VALUES
+    (1,'1000 corn','1000 corn got'),
+    (2,'500 chickens','500 chickens got'),
+    (3,'10 granaries','10 granaries built');
--- src/main/resources/db_scripts/13_Trigger_Notifications.sql
+++ src/main/resources/db_scripts/13_Trigger_Notifications.sql
@@ -2,6 +2,6 @@ INSERT INTO `Trigger_Notification`
     (`id`, `notification_id`,`building_id`,`resource_id`,`upgrade_id`,
                         `building_number`,`resource_number`,`upgrade_number`)
 VALUES
-  ('1','1','1','1','1','1','1','1'),
-  ('2','2','2','2','2','2','2','2'),
-  ('3','3','3','3','3','3','3','3');
+  (1,1,1,1,1,1,1,1),
+  (2,2,2,2,2,2,2,2),
+  (3,3,3,3,3,3,3,3);
