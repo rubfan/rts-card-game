1ace32b9c8a3097a2e20845057e61b3a882a5dd4
rickdemonn <rickdemonn@gmail.com>
2018-06-18 21:18:57 +0300

    AccountResorcesQuantity

--- src/main/java/game/repositories/dao/impl/AccountResourceDaoImpl.java
+++ src/main/java/game/repositories/dao/impl/AccountResourceDaoImpl.java
@@ -3,6 +3,7 @@ package game.repositories.dao.impl;
 import game.repositories.dao.AccountResourceDao;
 import game.repositories.dao.helpers.QueryHelper;
 import game.repositories.entities.AccountResourceEntity;
+import game.repositories.entities.AccountResourceQuantityEntity;
 
 import java.sql.*;
 import java.util.LinkedList;
@@ -43,6 +44,41 @@ public class AccountResourceDaoImpl implements AccountResourceDao {
             }
         }.run();
 
+    }
+
+    @Override
+    public List<AccountResourceQuantityEntity> getAccountResourcesQuantity(Integer accountId){
+        return new QueryHelper<List<AccountResourceQuantityEntity>>() {
+            protected void executeQuery(Statement statement, Connection connection) throws SQLException {
+                List<AccountResourceQuantityEntity> accountResourceQuantityList = new LinkedList<>();
+                ResultSet rs = statement.executeQuery(prepareListOfAllowCardsForAccountQuery(accountId));
+                while (rs.next()) {
+                    AccountResourceQuantityEntity accountResourceQuantity = new AccountResourceQuantityEntity(
+                    rs.getInt("resource_id"),
+                    rs.getInt("number"),
+                    rs.getInt("res_per_min"));
+                    accountResourceQuantityList.add(accountResourceQuantity);
+                }
+                if (accountResourceQuantityList.size() > 0) {
+                    returnResult(accountResourceQuantityList);
+                }
+            }
+        }.run();
+    }
+
+    private String prepareListOfAllowCardsForAccountQuery(Integer accountId) {
+        StringBuilder q = new StringBuilder();
+        q.append("SELECT ar.resource_id, ar.number, (ab.number * bp.number_per_sec + ab.number / 100 * up.percent) res_per_min ");
+        q.append("FROM Account_Resource ar ");
+        q.append("LEFT JOIN Account_Building ab ON ar.account_id = ab.account_id ");
+        q.append("LEFT JOIN Account_Upgrade au ON ar.account_id = au.account_id ");
+        q.append("LEFT JOIN Building_Product bp ON bp.building_id = ab.building_id ");
+        q.append("LEFT JOIN Upgrade_Product up ON up.upgrade_id = au.upgrade_id ");
+        q.append("WHERE ar.account_id = " + accountId +
+                 " AND ab.account_id = " + accountId);
+        q.append(" AND bp.resource_id = ar.resource_id AND ab.building_id = bp.building_id ");
+        q.append("GROUP BY bp.resource_id ");
+        return q.toString();
     }
 
 }
